aaea9e43d44bb0d50b7af41a69b761c7aea7b7f4
Waffiyyi <fasholawafiyyi@gmail.com>
2024-10-11 18:29:55 +0100

    updated backend url

--- app/config/api.js
+++ app/config/api.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-export const API_URL = "https://book-market-place-backend.onrender.com";
+export const API_URL = "https://simple-rasia-waffiyyidev-5e895312.koyeb.app";
 export const API_URL_LOCAL = "http://localhost:8015";
 
 export const api = axios.create(
