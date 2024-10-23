95cdf01097b92f25118f6b09517e31a7449c2758
Waffiyyi <fasholawafiyyi@gmail.com>
2024-10-23 20:21:55 +0100

    updated backend url

--- app/config/api.js
+++ app/config/api.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-export const API_URL = "https://simple-rasia-waffiyyidev-5e895312.koyeb.app";
+export const API_URL = "https://favourable-beatriz-bookmarket-798e097d.koyeb.app";
 export const API_URL_LOCAL = "http://localhost:8015";
 
 export const api = axios.create(
